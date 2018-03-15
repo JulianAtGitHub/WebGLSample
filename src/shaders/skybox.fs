bf8fa1f8f2726cfe0f468e44c2cd9aac055b0048
zhuwei <zhuwei@MBP-00173.zhuwei>
2018-03-15 21:08:14 +0800

    Enable HDR tone map when draw skybox

--- src/shaders/skybox.fs
+++ src/shaders/skybox.fs
@@ -8,7 +8,7 @@ void main(void) {
   vec3 envColor = textureCube(uEnvMap, vPosition).rgb;
 
   // HDR tonemap
-  // envColor = envColor / (envColor + vec3(1.0));
+  envColor = envColor / (envColor + vec3(1.0));
   // gamma correction
   envColor = pow(envColor, vec3(1.0 / 2.2));
 

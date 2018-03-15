622b5a2f7b14a3f1052619560ee6ae7fd366412f
Julian <zw616636719@gmail.com>
2018-03-15 18:08:38 +0800

    Disable tonemap when draw skybox with HDR texture

--- src/shaders/skybox.fs
+++ src/shaders/skybox.fs
@@ -1,3 +1,5 @@
+precision mediump float;
+
 varying vec3 vPosition;
 
 uniform samplerCube uEnvMap;
@@ -6,7 +8,7 @@ void main(void) {
   vec3 envColor = textureCube(uEnvMap, vPosition).rgb;
 
   // HDR tonemap
-  envColor = envColor / (envColor + vec3(1.0));
+  // envColor = envColor / (envColor + vec3(1.0));
   // gamma correction
   envColor = pow(envColor, vec3(1.0 / 2.2));
 
